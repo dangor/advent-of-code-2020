26ab29dd9b900a772707ea6563fce056a465c77c
Brian <briancdang@gmail.com>
2020-12-15 05:14:48 +0000

    Day 15 problem 2

---
+++ day15/p2.py
@@ -0,0 +1,19 @@
+def run():
+  input = [2,20,0,4,1,17]
+  tracker = {}
+
+  for i in range(len(input) - 1):
+    tracker[input[i]] = i
+
+  current = input[-1]
+    
+  for i in range(len(input) - 1, 30000000 - 1):
+    if current not in tracker:
+      tracker[current] = i
+      current = 0
+    else:
+      diff = i - tracker[current]
+      tracker[current] = i
+      current = diff
+
+  print(f"Answer: {current}")
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import day11.p1, day11.p2
 import day12.p1, day12.p2
 import day13.p1, day13.p2
 import day14.p1, day14.p2
-import day15.p1
+import day15.p1, day15.p2
 
 print('day 1 problem 1')
 day01.p1.run('day01/input.txt')
@@ -102,3 +102,7 @@ day14.p2.run('day14/input.txt')
 
 print('\nday 15 problem 1')
 day15.p1.run()
+
+# slow running
+# print('\nday 15 problem 2')
+# day15.p2.run()
